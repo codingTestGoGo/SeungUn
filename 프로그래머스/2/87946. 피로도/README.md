56cebc91d49c195f52da70c8c98e590aeea53978
이승언 <dobana12345@naver.com>
2026-02-16 15:15:22 +0900

    [level 2] Title: 피로도, Time: 1.20 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/README.md
+++ 프로그래머스/2/87946. 피로도/README.md
@@ -1,10 +1,10 @@
 # [level 2] 피로도 - 87946 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946) 
 
 ### 성능 요약
 
-메모리: 3.65 MB, 시간: 0.97 ms
+메모리: 4.2 MB, 시간: 1.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 02월 16일 15:11:45
+2026년 02월 16일 15:15:23
 
 ### 문제 설명
 
--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,48 +1,27 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <iostream>
 
 using namespace std;
 
-int maxCount = 0;
+int answer = 0;
 
-bool backtracking(int k, int current, vector<bool> &visited, int visitedCount, const vector<vector<int>> &dungeons)
-{
-    visited[current] = true;
-    visitedCount += 1;
-    k -= dungeons[current][1];
-    //cout << "visit " << current << '\n';
-    
-    for (int i = 0; i < visited.size(); i++)
-    {
-        if (!visited[i] && k >= dungeons[i][0])
-        {
-            if (backtracking(k, i, visited, visitedCount, dungeons))
-                return true;
+void dfs(int k, int count, vector<bool> &visited, const vector<vector<int>> &dungeons) {
+    answer = max(answer, count);
+
+    for (int i = 0; i < dungeons.size(); i++) {
+        if (!visited[i] && k >= dungeons[i][0]) {
+            visited[i] = true;
+            dfs(k - dungeons[i][1], count + 1, visited, dungeons);
+            visited[i] = false;
         }
     }
-    
-    maxCount = max(maxCount, visitedCount);
-    //cout << maxCount << '\n';
-    
-    visited[current] = false;
-    return false;
 }
 
 int solution(int k, vector<vector<int>> dungeons) {
-    sort(dungeons.begin(), dungeons.end(), [](const vector<int> &a, const vector<int> &b)
-    {
-        return a[0] < b[0];
-    });
-    
+    answer = 0;
     vector<bool> visited(dungeons.size(), false);
-    for (int i = 0; i < dungeons.size(); i++)
-    {
-        
-        backtracking(k, i, visited, 0, dungeons);
-        //cout << '\n';
-    }
+    dfs(k, 0, visited, dungeons);
     
-    return maxCount;
+    return answer;
 }
