58b3d1cefa5eae03b9cb738b91cc195593a31929
이승언 <dobana12345@naver.com>
2026-03-24 17:04:37 +0900

    [level 3] Title: 양과 늑대, Time: 0.30 ms, Memory: 3.69 MB -BaekjoonHub

--- 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.cpp
@@ -5,45 +5,46 @@
 
 using namespace std;
 
-vector<vector<int>> tree;
-vector<int> info_v;
-int ans = 0;
-
-void dfs(vector<int> next_nodes, int sheep, int wolf)
-{
-    ans = max(ans, sheep);
-    //printf("ans = %d, sheep = %d, wolf = %d\n", ans, sheep, wolf);
-    for (int i = 0; i < next_nodes.size(); i++)
-    {
-        int node = next_nodes[i];
-        
-        const int sh = sheep + (info_v[node] ? 0 : 1);
-        const int wf = wolf + (info_v[node] ? 1 : 0);
-        if (sheep <= wolf) continue;
-        
-        vector<int> temp(next_nodes.begin(), next_nodes.end());
-        swap(temp[i], temp[temp.size()-1]);
-        temp.pop_back();
-        for (int next : tree[node])
-        {
-            temp.push_back(next);
-        }
-        
-        dfs(temp, sh, wf);
-    }
-}
-
-
 int solution(vector<int> info, vector<vector<int>> edges)
 {
-    info_v = info;
-    tree.resize(info.size());
+    int answer = 0;
+    int n = info.size();
+    vector<vector<int>> tree(n);
     for (const vector<int> &edge : edges)
     {
         const int n1 = edge[0], n2 = edge[1];
         tree[n1].push_back(n2);
     }
     
-    dfs(tree[0], 1, 0);
-    return ans;
+    auto dfs = [&](int state, int sheep, int wolf, auto&& dfs) -> void
+    {
+        answer = max(answer, sheep);    
+        for (int i = 0; i < n; i++)
+        {
+            if (state & (1 << i))
+            {
+                const int sh = sheep + (info[i] ? 0 : 1);
+                const int wf = wolf + (info[i] ? 1 : 0);
+                
+                if (sh <= wf) continue;
+                
+                int newState = state;
+                newState &= ~(1 << i);
+                for (int next : tree[i])
+                {
+                    newState |= (1 << next);
+                }
+                
+                dfs(newState, sh, wf, dfs);
+            }
+        }
+    };
+    
+    int init = 0;
+    for (int next : tree[0])
+    {
+        init |= (1 << next);
+    }
+    dfs(init, 1, 0, dfs);
+    return answer;
 }
